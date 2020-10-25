e62671bed95234a3b450a7dc63898abaee607c3a
tanishq1010 <73070362+tanishq1010@users.noreply.github.com>
2020-10-25 14:26:36 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import  pandas as pd
+import traceback
 
 df1=pd.read_csv("Chapter_Questions_1.csv")
 df2=pd.read_csv("Chapter_Questions_2.csv")
@@ -12,15 +13,21 @@ df.to_csv("Chapter_Questions.csv",index=False)
 df=pd.read_csv("Question_bank.csv").drop_duplicates()
 df1 = pd.read_csv("Chapter_Questions.csv").drop_duplicates()
 df2 = pd.read_csv("Chapter_hygiene.csv").drop_duplicates()
+
+print(int(" "))
 list1 = [""] * len(df1)
 df1["Present in CG"] = list1
 for ind in df1.index:
+ try:
     print(int(df1["Question Id"][ind]))
     df_new = df.loc[df["id"] == int(df1["Question Id"][ind])]
     if len(df_new) > 0:
         df1["Present in CG"][ind] = "yes"
     else:
         df1["Present in CG"][ind] = "no"
+ except Exception as e:
+     print(traceback.format_exc())
+     df1["Present in CG"][ind] = ""
 df1.to_csv("Chapter_Questions.csv", index=False)
 
 list1 = [""] * len(df2)
